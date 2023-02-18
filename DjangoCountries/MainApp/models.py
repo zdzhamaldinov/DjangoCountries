2f244c599cbec67127a6f366b02eb518eb1f5455
Zaur D <z.dzhamaldinov@gmail.com>
2023-02-19 02:58:07 +0600

    Модуль 4

--- DjangoCountries/MainApp/models.py
+++ DjangoCountries/MainApp/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class Language(models.Model):
+   name = models.CharField(max_length=100)
+   
+   def __str__(self):
+    return self.name
+
 class Country(models.Model):
    country = models.CharField(max_length=500)
-   languages = models.TextField()
+   languages = models.ManyToManyField(to=Language)
+   
+   def __str__(self):
+    return self.country
--- DjangoCountries/MainApp/views.py
+++ DjangoCountries/MainApp/views.py
@@ -2,7 +2,7 @@ import json
 from django.shortcuts import render, HttpResponse
 from django.http import HttpResponseNotFound
 from django.core.exceptions import ObjectDoesNotExist
-from MainApp.models import Country
+from MainApp.models import Country, Language
 
 
 def home(request):
@@ -19,7 +19,8 @@ def get_country(request,name):
     except ObjectDoesNotExist:
         return HttpResponseNotFound(f"Country with name={name} not found")
     context = {
-        'country': country
+        'country': country.country,
+        'languages': ', '.join(list(country.languages.all().values_list('name', flat=True))),
     }
     return render(request, 'country.html', context)
 
@@ -30,11 +31,9 @@ def get_countries_list(request):
     }
     return render(request, "countries_list.html", context)
     
+    
 def get_languages_list(request):
-    languages_list = []
-    for country in Country.objects.all():
-        languages_list += country.languages.split(',')
     context = {
-        "languages_list": languages_list,
+        "languages_list": Language.objects.all(),
     }
     return render(request, "languages_list.html", context)
